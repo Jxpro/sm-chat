8a8caad2aaa42570e59ba592f65836c4ed7e9f8f
jokerxin <jxpro@qq.com>
2022-07-03 22:27:56 +0800

    Remove unused files

--- common/config.py
+++ common/config.py
@@ -4,7 +4,6 @@
 """获取配置文件信息"""
 
 import json
-from pprint import pprint
 
 with open('config.json') as config_file:
     config = json.load(config_file)
--- common/global_vars.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-"""全局变量"""
-
-# crypto中的共享密钥
-shared_secret = ''
