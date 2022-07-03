91c043ff4db1d879866ed454ee1ac561af5e868d
jokerxin <jxpro@qq.com>
2022-07-03 17:28:55 +0800

    Format contract form

--- client/forms/contacts_form.py
+++ client/forms/contacts_form.py
@@ -2,37 +2,70 @@
 # -*- coding:utf-8 -*-
 
 """联系人列表"""
-import _tkinter
-import tkinter as tk
-from distutils import command
-from tkinter import messagebox
-from common.message import MessageType, _deserialize_any
-from pprint import pprint
-import client.memory
-import select
-import _thread
-from tkinter import *
-from client.components.vertical_scrolled_frame import VerticalScrolledFrame
-from client.components.contact_item import ContactItem
-from client.forms.chat_form import ChatForm
-from tkinter import Toplevel
 import datetime
-import client.util.socket_listener
-import time
+import tkinter as tk
+from tkinter import *
+from tkinter import Toplevel
+from tkinter import messagebox
 from tkinter import simpledialog
+
+import client.memory
+import client.util.socket_listener
+from client.components.contact_item import ContactItem
+from client.components.vertical_scrolled_frame import VerticalScrolledFrame
+from client.forms.chat_form import ChatForm
+from common.message import MessageType, _deserialize_any
 
 
 class ContactsForm(tk.Frame):
     bundle_process_done = False
 
-    """#关闭contacts_form"""
+    def __init__(self, master=None):
+        client.memory.contact_window.append(self)
+        super().__init__(master)
+        self.master = master
+        self.master.title(client.memory.current_user['username'] + "——联系人列表")
+        master.resizable(width=False, height=False)
+        master.geometry('400x640')
+        # 滚动条＋消息列表画布
+        self.scroll = VerticalScrolledFrame(self)
+        self.scroll.pack(side=TOP, fill=BOTH, expand=True)
+        # 按钮
+        self.button_frame_left = Frame(self)
+        self.button_frame_left.pack(side=LEFT, fill=BOTH, expand=YES)
+        self.button_frame_right = Frame(self)
+        self.button_frame_right.pack(side=RIGHT, fill=BOTH, expand=YES)
+        # 添加好友
+        self.add_friend = Button(self.button_frame_left, text="添加好友", font=("楷体", 16), fg="black", bg="#35d1e9",
+                                 activebackground="#6cdcf0", relief=GROOVE, command=self.on_add_friend)
+        self.add_friend.pack(side=TOP, expand=True, fill=BOTH)
+        # 添加群聊
+        self.add_room = Button(self.button_frame_left, text="添加群聊", font=("楷体", 16), fg="black", bg="#35d1e9",
+                               activebackground="#6cdcf0", relief=GROOVE, command=self.on_add_room)
+        self.add_room.pack(side=TOP, expand=True, fill=BOTH)
+        # 删除好友
+        self.del_friend = Button(self.button_frame_right, text="删除好友", font=("楷体", 16), fg="black", bg="#35d1e9",
+                                 activebackground="#6cdcf0", relief=GROOVE, command=self.on_del_friend)
+        self.del_friend.pack(side=TOP, expand=True, fill=BOTH)
+        # 创建群聊
+        self.create_room = Button(self.button_frame_right, text="创建群聊", font=("楷体", 16), fg="black", bg="#35d1e9",
+                                  activebackground="#6cdcf0", relief=GROOVE, command=self.on_create_room)
+        self.create_room.pack(side=TOP, expand=True, fill=BOTH)
+        # 页面定位
+        self.pack(side=TOP, fill=BOTH, expand=True)
+        self.contacts = []
+        self.sc = client.memory.sc
+        client.util.socket_listener.add_listener(self.socket_listener)
+        master.protocol("WM_DELETE_WINDOW", self.remove_socket_listener_and_close)
+
     def remove_socket_listener_and_close(self):
+        """#关闭contacts_form"""
         client.util.socket_listener.remove_listener(self.socket_listener)
         self.master.destroy()
         client.memory.tk_root.destroy()
 
-    """监听从服务端发来的反馈"""
     def socket_listener(self, data):
+        """监听从服务端发来的反馈"""
         if data['type'] == MessageType.login_bundle:
             bundle = data['parameters']
             friends = bundle['friends']
@@ -88,21 +121,20 @@ class ContactsForm(tk.Frame):
             self.refresh_contacts()
             return
 
-    """处理新的联系人"""
     def handle_new_contact(self, data):
+        """处理新的联系人"""
         data['last_timestamp'] = 0
         data['last_message'] = '(没有消息)'
         self.contacts.insert(0, data)
         self.refresh_contacts()
 
-    """处理删除好友的操作后"""
     def handle_del_contact(self, data):
+        """处理删除好友的操作后"""
         id = data['id']
         for conn in self.contacts:
             if (conn['id'] == id):
                 self.contacts.remove(conn)
         self.refresh_contacts()
-
 
     def on_frame_click(self, e):
         item_id = e.widget.item['id']
@@ -112,34 +144,34 @@ class ContactsForm(tk.Frame):
         form = Toplevel(client.memory.tk_root, takefocus=True)
         client.memory.window_instance[e.widget.item['type']][item_id] = ChatForm(e.widget.item, form)
 
-    """ 添加好友 """
     def on_add_friend(self):
+        """ 添加好友 """
         result = simpledialog.askstring('添加好友', '请输入用户名')
         if (not result):
             return
         self.sc.send(MessageType.add_friend, result)
 
-    """ 删除好友 """
     def on_del_friend(self):
+        """ 删除好友 """
         result = simpledialog.askstring('删除好友', '请输入用户名')
         if (not result):
             return
         self.sc.send(MessageType.del_friend, result)
-        #print(MessageType.del_friend)
+        # print(MessageType.del_friend)
 
-    """ 查看用户信息 """
     def on_user_information(self):
+        """ 查看用户信息 """
         return
 
-    """ 添加群 """
     def on_add_room(self):
+        """ 添加群 """
         result = simpledialog.askinteger('添加群', '请输入群号')
         if (not result):
             return
         self.sc.send(MessageType.join_room, result)
 
-    """" 创建群 """
     def on_create_room(self):
+        """" 创建群 """
         result = simpledialog.askstring('创建群', '请输入群名称')
         if (not result):
             return
@@ -147,11 +179,12 @@ class ContactsForm(tk.Frame):
 
     class my_event:
         widget = None
+
         def __init__(self, widget):
             self.widget = widget
 
-    """ 查看用户ID """
     def try_open_user_id(self, id, name, username):
+        """ 查看用户ID """
         for i in range(0, len(self.pack_objs)):
             frame = self.pack_objs[i]
             if frame.item['id'] == id and frame.item['type'] == 0:
@@ -160,11 +193,12 @@ class ContactsForm(tk.Frame):
         result = messagebox.askyesno("是否加好友", name + "不在您的好友列表中，是否加好友？")
         if result:
             self.sc.send(MessageType.add_friend, username)
+
     pack_objs = []
 
-    """更新列表界面"""
     def refresh_contacts(self):
-        #print("self.contacts是一个列表，里面很多用户字典")
+        """更新列表界面"""
+        # print("self.contacts是一个列表，里面很多用户字典")
         if not self.bundle_process_done:
             return
 
@@ -221,41 +255,3 @@ class ContactsForm(tk.Frame):
                 contact.unread_message_count.pack(side=RIGHT, anchor=E, fill=None, expand=False, ipadx=4)
                 contact.last_message.pack(side=LEFT, fill=X, expand=True, anchor=W)
                 contact.unread_message_count.config(text=str(unread_count))
-
-    def __init__(self, master=None):
-        client.memory.contact_window.append(self)
-        super().__init__(master)
-        self.master = master
-        self.master.title(client.memory.current_user['username'] + "——联系人列表")
-        master.resizable(width=False, height=False)
-        master.geometry('400x640')
-        # 滚动条＋消息列表画布
-        self.scroll = VerticalScrolledFrame(self)
-        self.scroll.pack(side=TOP, fill=BOTH, expand=True)
-        # 按钮
-        self.button_frame_left = Frame(self)
-        self.button_frame_left.pack(side=LEFT, fill=BOTH, expand=YES)
-        self.button_frame_right = Frame(self)
-        self.button_frame_right.pack(side=RIGHT, fill=BOTH, expand=YES)
-        # 添加好友
-        self.add_friend = Button(self.button_frame_left, text="添加好友", font=("楷体", 16), fg="black", bg="#35d1e9",
-                                 activebackground="#6cdcf0", relief=GROOVE, command=self.on_add_friend)
-        self.add_friend.pack(side=TOP, expand=True, fill=BOTH)
-        # 添加群聊
-        self.add_room = Button(self.button_frame_left, text="添加群聊", font=("楷体", 16), fg="black", bg="#35d1e9",
-                               activebackground="#6cdcf0", relief=GROOVE, command=self.on_add_room)
-        self.add_room.pack(side=TOP, expand=True, fill=BOTH)
-        # 删除好友
-        self.del_friend = Button(self.button_frame_right, text="删除好友", font=("楷体", 16), fg="black", bg="#35d1e9",
-                                 activebackground="#6cdcf0", relief=GROOVE, command=self.on_del_friend)
-        self.del_friend.pack(side=TOP, expand=True, fill=BOTH)
-        # 创建群聊
-        self.create_room = Button(self.button_frame_right, text="创建群聊", font=("楷体", 16), fg="black", bg="#35d1e9",
-                                  activebackground="#6cdcf0", relief=GROOVE, command=self.on_create_room)
-        self.create_room.pack(side=TOP, expand=True, fill=BOTH)
-        # 页面定位
-        self.pack(side=TOP, fill=BOTH, expand=True)
-        self.contacts = []
-        self.sc = client.memory.sc
-        client.util.socket_listener.add_listener(self.socket_listener)
-        master.protocol("WM_DELETE_WINDOW", self.remove_socket_listener_and_close)
